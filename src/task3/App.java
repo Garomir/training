f5adf5c4e4da615ec4e64f60b47e12a812fbab32
Garomir <ramilfin@mail.ru>
2021-11-22 12:14:49 +0300

    task3

--- src/task3/App.java
+++ src/task3/App.java
@@ -1,7 +1,39 @@
 package task3;
 
 public class App {
-    public static void main(String[] args) {
+    public static int[] arr = new int[30];
+    public static int testInt = 5;
 
+    public static void main(String[] args) {
+        //Дан массив целых чисел и ещё одно целое число.
+        //Удалите все вхождения этого числа из массива (пропусков быть не должно).
+        init();
+        displayArr();
+        removeTestInt();
+        displayArr();
+    }
+
+    public static void init(){
+        for (int i = 0; i < 30; i++) {
+            arr[i] = (int) (Math.random() * 8)+1;
+        }
+    }
+
+    public static void removeTestInt(){
+        for (int i = 0; i < 30; i++) {
+            if (arr[i] == testInt){
+                for (int j = i+1; j < arr.length-1; j++) {
+                    arr[j-1] = arr[j];
+                    arr[j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void displayArr(){
+        for (int i = 0; i < arr.length-1; i++) {
+            System.out.print(arr[i] + ",");
+        }
+        System.out.println();
     }
 }
